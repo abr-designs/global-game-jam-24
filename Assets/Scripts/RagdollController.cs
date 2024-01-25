4d0b5cb2a571e7b7c9fb2140a8925b7f957b8d92
Alex Bedard-Reid <alexbedardreid@gmail.com>
2024-01-24 18:27:22 -0800

    Explosion Based Movement

--- Assets/Scripts/RagdollController.cs
+++ Assets/Scripts/RagdollController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RagdollController : MonoBehaviour
 {
@@ -29,11 +30,22 @@ public class RagdollController : MonoBehaviour
     private float _groundHeightOffset;
     private float _heightOffGround;
 
+    [Min(0f)]
+    public float mult = 10f;
+
+    private Rigidbody[] _rigidbodies;
+
+    [SerializeField]
+    private Transform sphereTransform;
+    [SerializeField]
+    private Material material;
+
     //============================================================================================================//
 
     private void OnEnable()
     {
         PuppetRagdoll.OnRagdollActive += OnRagdollActive;
+        _rigidbodies = GetComponentsInChildren < Rigidbody>();
     }
 
 
@@ -41,54 +53,63 @@ public class RagdollController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        CheckGroundHeight(out _groundHeightOffset);
-        _groundHeightOffset *= 0.95f;
+        //CheckGroundHeight(out _groundHeightOffset);
+        //_groundHeightOffset *= 0.95f;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (_ragdollActive)
-            return;
-        
-        UpdateInputDirection(ref _inputDirections);
-        CheckGroundHeight(out _heightOffGround);
-
-        _hasInput = _inputDirections != Vector2.zero;
-
-        if (_hasInput == false)
+        const float radius = 2.5f;
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            puppeteerAnimator.SetFloat(SpeedAnimator, 0f);
+            SceneManager.LoadScene(0);
             return;
         }
         
-        puppeteerAnimator.SetFloat(SpeedAnimator, speed);
-
-        var dir = GetCameraMoveDirection(cameraTransform, _inputDirections);
-
-        var rotation = Quaternion.LookRotation(dir);
-
-        root.transform.rotation = rotation;
-
-        var newPosition = root.transform.position;
-        newPosition.y = _heightOffGround;
-        newPosition += dir * (speed * Time.deltaTime);
-
-        root.transform.position = newPosition;
-    }
-
-    /*private void FixedUpdate()
-    {
-        if (hasInput == false)
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        
+        if (Physics.Raycast(ray, out var raycastHit, 100, groundMask.value) == false)
             return;
 
-        var dir = GetCameraMoveDirection(cameraTransform, inputDirections);
+        sphereTransform.position = raycastHit.point;
 
-        var rotation = Quaternion.LookRotation(dir);
+        var color1 = Color.cyan;
+        if (Input.GetKey(KeyCode.Mouse0) == false)
+        {
+            
+            color1.a = 0.1f;
+            material.SetColor("_Color", color1);
+            sphereTransform.localScale = Vector3.one * (radius / 3f);
+            return;
+        }
         
-        //root.MoveRotation(rotation);
-        root.position += dir * (speed);
-    }*/
+        color1.a = 0.5f;
+        material.SetColor("_Color", color1);
+        sphereTransform.localScale = Vector3.one * (radius * 2f);
+
+        for (int i = 0; i < _rigidbodies.Length; i++)
+        {
+            _rigidbodies[i].AddExplosionForce(mult, raycastHit.point, radius, 2f);
+        }
+
+    }
+
+    private void MouseMove()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var raycastHit, 100, groundMask.value) == false)
+            return;
+
+
+        Debug.DrawLine(ray.origin, raycastHit.point, Color.green, 1f);
+        var dif = root.transform.position - raycastHit.point;
+        root.transform.position = Vector3.MoveTowards(root.transform.position,
+            raycastHit.point,
+            dif.magnitude * mult * Time.deltaTime);
+    }
     
     private void OnDisable()
     {
