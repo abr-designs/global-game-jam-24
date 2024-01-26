60f90a3a85e41506cae2ea1e320aec0220dfbd05
Alex Bedard-Reid <alexbedardreid@gmail.com>
2024-01-25 18:16:17 -0800

    Organized Project Structure & Added Randalls assets

---
+++ Assets/Prototype/Randall/Scripts/ScoringSystem/PointsPopupManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsPopupManager : MonoBehaviour {
+
+    [SerializeField] private GameObject pointsPopupPrefab;
+
+    [SerializeField] private bool clearExistingPopups = true;
+
+    private void Start() {
+
+        GameScoreManager.Instance.OnPointsScored += GameScoreManager_OnPointsScored;
+
+    }
+
+    private void GameScoreManager_OnPointsScored(object sender, ScorePointsArgs args) {
+
+        if(clearExistingPopups) {
+            foreach(Transform child in transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        GeneratePopup(args);
+
+    }
+
+    private void GeneratePopup(ScorePointsArgs args) {
+        
+        GameObject newPopup = Instantiate(pointsPopupPrefab, transform);
+        PointsPopup pointsPopup = newPopup.GetComponent<PointsPopup>();
+
+        pointsPopup.InitializeVisual(args);
+    }
+}
