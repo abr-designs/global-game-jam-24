738773ef6cbcb7efa270f5888dd9bbbb4a7fba1e
Alex Bedard-Reid <alexbedardreid@gmail.com>
2024-01-28 08:48:33 -0800

    Basic Cinematic Integration

---
+++ Assets/Scripts/Cinematics/IntroCinematic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Cinematics
+{
+    public class IntroCinematic : CinematicBase
+    {
+        public override bool IsPlaying { get; protected set; }
+
+        [SerializeField, Min(0)]
+        private float delay;
+        
+        protected override IEnumerator PlayCinematicCoroutine()
+        {
+            IsPlaying = true;
+            ForceSetCameraPosition(targetCamera);
+            
+            //TODO Get all items to avoid & tell the player
+
+            yield return new WaitForSeconds(delay);
+
+
+            
+            IsPlaying = false;
+        }
+    }
+}
